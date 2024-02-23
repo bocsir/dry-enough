e768798bcb84f1e432a096a54069e50475c9dbb1
bocsir <jacobwharris13@gmail.com>
2024-02-23 00:43:55 -0500

    emojis, gradient stops on load

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-// require('dotenv').config();
+const express = require('express');
+const fetch = require('node-fetch');
+const app = express();
+const port = 3000;
 
-// const express = require('express');
-// const app = express();
-// const port = 3000;
+require('dotenv').config();
+const apiKey = process.env.API_KEY;
 
-// const apiKey = process.env.API_KEY;
+app.length('/', (req, res) => {
+    res.send('helloworld');
+});
+
+app.listen(port, () => {
+    console.log('server is running at http://localhost:${port}');
+});
