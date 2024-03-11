a798e23a7f4bf5ab4e013b820b36346b3e023ca7
bocsir <jacobwharris13@gmail.com>
2024-03-11 18:14:24 -0400

    -

---
+++ public/script.js
@@ -0,0 +1,231 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("weather-results").style.display = "none";
+  if(document.getElementById('temp-unit').textContent === '°C') {
+    document.getElementById('temp-unit').style.left = '29px';
+    console.log('it should be on the right side onow i hop');
+    
+  }
+
+});
+
+let formLoaded = false;
+let day = 0;
+
+let temperatureData = [
+  { day: '1', maxFahrenheit: null, minFahrenheit: null, maxCelsius: null, minCelsius: null },
+  { day: '2', maxFahrenheit: null, minFahrenheit: null, maxCelsius: null, minCelsius: null },
+  { day: '3', maxFahrenheit: null, minFahrenheit: null, maxCelsius: null, minCelsius: null },
+  { day: '4', maxFahrenheit: null, minFahrenheit: null, maxCelsius: null, minCelsius: null },
+  { day: '5', maxFahrenheit: null, minFahrenheit: null, maxCelsius: null, minCelsius: null },
+  { day: '6', maxFahrenheit: null, minFahrenheit: null, maxCelsius: null, minCelsius: null }
+];
+
+const socket = new WebSocket('ws://localhost:5500');
+
+async function displayWeather(data, day) {
+  apiData = data;
+  document.getElementById("weather-grid").style.display = "grid";
+
+  //set results header
+  const resultsHeader = document.getElementById("results-header");
+  const locationName = data.location.name;
+  const locationRegion = data.location.region;
+  let locationCountry = data.location.country;
+  if (locationCountry === 'United States of America') { locationCountry = 'USA'; }
+  resultsHeader.innerHTML = "Past weather for " + locationName + ", " + locationRegion + ", " + locationCountry + ":";
+
+  const weatherItem = document.getElementById("day" + day);
+
+  //set weather icon
+  const weatherIcon = weatherItem.querySelector(".weather-icon");
+  const iconText = data.forecast.forecastday[0].day.condition.text;
+  // console.log(iconText);
+  setWeatherIcon(iconText, weatherIcon);
+
+  //set dates
+  const weatherDate = weatherItem.querySelector(".date");
+  const formattedDate = formatDate(data.forecast.forecastday[0].date);
+  weatherDate.innerHTML = formattedDate;
+
+  const weatherData = weatherItem.querySelector(".weather-data");
+
+  //set percipitation
+  const precipInches = data.forecast.forecastday[0].day.totalprecip_in;
+  const percipData = weatherData.querySelector(".percip-data");
+  percipData.innerHTML = " " + precipInches + " in.";
+
+  //set max temp
+  const maxTempF = weatherData.querySelector(".max-temp");
+
+  //set min temp
+  const minTempF = weatherData.querySelector(".min-temp");
+
+  //set temperature based on toggle switch position
+  if(document.getElementById('temp-unit').textContent === '°F') {
+    maxTempF.innerHTML = data.forecast.forecastday[0].day.maxtemp_f;
+    minTempF.innerHTML = data.forecast.forecastday[0].day.mintemp_f;
+  } else {
+    maxTempF.innerHTML = data.forecast.forecastday[0].day.maxtemp_c;
+    minTempF.innerHTML = data.forecast.forecastday[0].day.mintemp_c;
+  }
+
+  //put values in temperatureData
+  temperatureData[day-1].maxFahrenheit = data.forecast.forecastday[0].day.maxtemp_f;
+  temperatureData[day-1].minFahrenheit = data.forecast.forecastday[0].day.mintemp_f;
+  temperatureData[day-1].maxCelsius = data.forecast.forecastday[0].day.maxtemp_c;
+  temperatureData[day-1].minCelsius = data.forecast.forecastday[0].day.mintemp_c;
+
+  //show weather grid
+  document.getElementById("weather-results").style.display = "flex";
+
+  //stop gradient spin when form loaded
+  if (day === 6) { formLoaded = true; }
+
+  return { error: false }
+}
+
+//change temp unit on toggle switch, call toggleTempUnit() with new unit
+document.querySelector('.checkbox').addEventListener('click', () => {
+  //need to be able to chekc position of toggle switch instead of the current temp unit for page refresh when celsius
+  let tempUnitElement = document.getElementById('temp-unit');
+  let tempUnit;
+  if (tempUnitElement.textContent === '°F') { 
+    tempUnit = '°C';
+    tempUnitElement.style.left = '29px'; 
+    console.log('movin rigth');
+  } else { 
+    tempUnit = '°F'; 
+    tempUnitElement.style.left = '3px';
+    console.log('movin left');
+  }
+  tempUnitElement.textContent = tempUnit;
+  toggleTempUnit(tempUnit);
+});
+
+//change temp unit used in weather-grid
+function toggleTempUnit(u) {
+  for (let i = 1; i <= 6; i++) {
+    const weatherItem = document.getElementById("day" + i);
+    const weatherData = weatherItem.querySelector(".weather-data");
+    const maxTemp = weatherData.querySelector(".max-temp");
+    const minTemp = weatherData.querySelector(".min-temp");
+
+    if (u === '°F') {
+      maxTemp.innerHTML = temperatureData[i-1].maxFahrenheit;
+      minTemp.innerHTML = temperatureData[i-1].minFahrenheit;
+    } else {
+      maxTemp.innerHTML = temperatureData[i-1].maxCelsius;
+      minTemp.innerHTML = temperatureData[i-1].minCelsius;
+    }
+  }
+}
+
+async function submitForm() {
+  formLoaded = false;
+  gradientBorder();
+
+  const location = document.getElementById('location').value;
+  const formData = { location: location };
+
+  fetch('http://localhost:5500/submit', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(formData),
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Response from server:', data);
+    })
+    .catch(error => {
+      console.error('Error sending data:', error);
+    });
+}
+
+function setWeatherIcon(name, weatherIcon) {
+  const weatherIcons = {
+    '☀️': ["Sunny"],
+    '☁️': ["Cloudy", "Overcast"],
+    '🌨️': ["Moderate snow", "Patchy light snow", "Light snow", "Light snow showers"],
+    '🌧️': ["Light rain shower", "Light rain", "Patchy rain possible", "Moderate rain at times", "Light drizzle", "Light freezing rain", "Moderate rain", "Light sleet showers"],
+    '⛅': ["Partly cloudy"],
+    '❄️': ["Moderate or heavy snow showers", "Heavy snow", "Blizzard"],
+    '⛈️': ["Moderate or heavy rain with thunder", "Thundery outbreaks possible"],
+    '🌫️': ["Fog", "Freezing fog", "Mist"]
+  };
+
+  //find the common emoji for the given weather name
+  const emoji = Object.keys(weatherIcons).find(key => weatherIcons[key].includes(name)) || '';
+
+  weatherIcon.innerHTML = emoji;
+}
+
+//change order of date, not using year value
+function formatDate(date) {
+  let newDate = date.slice(2).replace(/-/g, "");
+
+  //day
+  const lastTwo = newDate.slice(-2);
+  //month
+  const middle = newDate.slice(2, -2);
+
+  newDate = middle + "<br>" + lastTwo;
+  return newDate;
+}
+
+//spin linear gradient until form is loaded
+async function gradientBorder() {
+  let borderElement = document.getElementById("form-border");
+
+  for (let i = 1; !formLoaded; i++) {
+    await new Promise(resolve => setTimeout(resolve));
+
+    const speedMultiplier = 5;
+    let deg = 30 + i * speedMultiplier;
+
+    borderElement.style.background =
+      "linear-gradient(" +
+      deg +
+      "deg, rgb(107, 20, 110) 30%, rgba(255, 255, 0, 0.671) 50%, rgb(109, 0, 0) 70%)";
+  }
+}
+
+function showError() {
+  document.getElementById("results-header").innerHTML = "Error: check your input";
+  document.getElementById("results-header").style.display = "block";
+  document.getElementById("weather-results").style.display = "flex";
+  document.getElementById("weather-grid").style.display = "none";
+  formLoaded = true;
+}
+
+socket.onerror = function (event) {
+  console.log('Connection error:', event);
+}
+
+socket.onopen = function (event) {
+  console.log('Connected to server');
+}
+
+socket.onclose = function (event) {
+  console.log('Connection closed', event);
+}
+
+const sendMessage = () => {
+  socket.send('hello server');
+}
+
+socket.onmessage = function (event) {
+  day++;
+  if (day === 7) { day = 1; }
+
+  const receivedMsg = event.data;
+  // console.log('Received message:', receivedMsg);
+
+
+  if (receivedMsg === 'bad response') {
+    showError();
+  } else {
+    displayWeather(JSON.parse(receivedMsg), day);
+  }
+}
--- server.js
+++ server.js
@@ -1,15 +1,100 @@
-const express = require('express');
-const fetch = require('node-fetch');
-const app = express();
-const port = 3000;
+//load modules:
+//require searches for specefied module in node_modules or Node.js core module
+const express = require('express'); //web framework for node with routing, middleware, ...
+const app = express(); //create instance of express.js application
+const fetch = require('node-fetch'); //node module for making http requests
+const path = require('path'); //node module for file path stuff
+const WebSocket = require('ws'); //web socket library for communication between client and server
+const bodyParser = require('body-parser');
 
+//headless websocket server
+const wss = new WebSocket.Server( {noServer: true} );
+const connectedSockets = {};
+
+//event listener for client connection to server
+wss.on('connection', (socket) => {
+    console.log('Client connected');
+
+    const clientId =  Date.now().toString();
+    connectedSockets[clientId] = socket;
+    //send test object to client on connection
+    // const testObj = {
+    //     message : 'hello client',
+    //     ok: true
+    // };  
+    // socket.send(JSON.stringify(testObj));
+
+    socket.on('close', () => {
+        console.log('Client disconnected');
+        //remove the socket from the connectedSockets object when it's closed
+        delete connectedSockets[clientId];
+    });
+});
+
+wss.on('error', console.error);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+//get API key from .env
 require('dotenv').config();
 const apiKey = process.env.API_KEY;
 
-app.length('/', (req, res) => {
-    res.send('helloworld');
+app.post('/submit', async (req, res) => {
+
+    console.log('form submitted');
+    
+    for (const clientId in connectedSockets) {
+        const socket = connectedSockets[clientId];
+        const location = req.body.location;
+        // make API calls for last 6 days
+        for (let i = 1; i <= 6; i++) {
+            const weatherApiUrl =
+            "http://api.weatherapi.com/v1/history.json?key=" +
+            apiKey +
+            "&q=" +
+            location +
+            "&unixdt=" +
+            (Math.floor(Date.now() / 1000) - 86400 * i);
+    
+            //make calls to weather api each iteration
+            try {
+                const response = await fetch(weatherApiUrl)
+                
+                if (!response.ok) {
+                    console.error("bad response: ", response);
+                    socket.send('bad response');
+                } else {
+                    const data = await response.json();
+                    //send the response to client
+                    socket.send(JSON.stringify(data));
+                }
+    
+            } catch (error) {
+                console.error("fetch error: ", error);
+                return { error: true }
+            }
+
+        }
+    }
+})
+
+// Define a route to handle the root URL
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-app.listen(port, () => {
-    console.log('server is running at http://localhost:${port}');
+
+const port = 5500;
+//start express server
+const server = app.listen(port, () => {
+    console.log(`Server is listening on http://localhost:${port}`);
+});
+
+//handle client request to upgrade to websocket
+server.on('upgrade', (request, socket, head) => {
+    wss.handleUpgrade(request, socket, head, socket => {
+      wss.emit('connection', socket, request);
+    });
 });
